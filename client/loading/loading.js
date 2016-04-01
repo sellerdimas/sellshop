ce181d79ed7bb38e6e6be4589cc9a609a6d90687
sellerdimas <sellerdimas@gmail.com>
2016-04-01 21:04:06 +0300

    razdelenyy

--- client/loading/loading.js
+++ client/loading/loading.js
@@ -5,7 +5,7 @@ Template.loading.rendered = function () {
 */var spinner = '<div class="sk-circle"><div class="sk-circle1 sk-child"></div><div class="sk-circle2 sk-child"></div><div class="sk-circle3 sk-child"></div><div class="sk-circle4 sk-child"></div><div class="sk-circle5 sk-child"></div><div class="sk-circle6 sk-child"></div><div class="sk-circle7 sk-child"></div><div class="sk-circle8 sk-child"></div><div class="sk-circle9 sk-child"></div><div class="sk-circle10 sk-child"></div><div class="sk-circle11 sk-child"></div><div class="sk-circle12 sk-child"></div></div>'
   if ( ! Session.get('loadingSplash') ) {
     this.loading = window.pleaseWait({
-      logo: 's.png',
+      logo: '/s.png',
       backgroundColor: '#FFFFFF',
       loadingHtml: spinner 
     });
